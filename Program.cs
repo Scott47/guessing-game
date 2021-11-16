62a68dd580ad0bf53a40a6ddc6f1b604366e6e41
scottsilver <scottsilver47@gmail.com>
2021-11-15 21:38:30 -0600

    initial commit & feat: user is prompted to guess number and program outputs that number to console

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace guessing_game
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Guess the secret number");
+            string input = Console.ReadLine();
+            int result = Int32.Parse(input);
+            Console.WriteLine(result);
+        }
+    }
+}
