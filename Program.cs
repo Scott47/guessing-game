8d748c5d6e48716af1438f3a4f40b1e1c89574ab
scottsilver <scottsilver47@gmail.com>
2021-11-15 21:44:54 -0600

    completed phase 2, user is prompted to guess a secret number and receives a failure or success msg depending on correctness of guess.

--- Program.cs
+++ Program.cs
@@ -9,7 +9,15 @@ namespace guessing_game
             Console.WriteLine("Guess the secret number");
             string input = Console.ReadLine();
             int result = Int32.Parse(input);
-            Console.WriteLine(result);
+            int secret_number = 42;
+            if (result == secret_number)
+            {
+                Console.WriteLine("Success, you guessed the secret number!");
+            }
+            else
+            {
+                Console.WriteLine("Failure");
+            }
         }
     }
 }
